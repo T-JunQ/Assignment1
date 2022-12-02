477d3516840498496368106637b8290a2a8b3926
T-JunQ <s10240058@connect.np.edu.sg>
2022-12-02 22:59:33 +0800

    2/11 Contact & footer finished

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+let enddate = new Date("Jan 5, 2023 00:00:00").getTime();
+
+
+
+
+let timer = setInterval(function(){
+    var now = new Date().getTime()
+
+    var diff = enddate-now
+
+    var d = Math.floor(diff/(1000*60*60*24));
+    var h = Math.floor(diff%(1000*60*60*24)/(1000*60*60));
+    var m = Math.floor(diff%(1000*60*60)/(1000*60));
+    var s = Math.floor(diff%(1000*60)/1000);
+
+    document.getElementById("timer").innerHTML 
+    = `${d}d ${h}h ${m}m ${s}s Left!`
+
+},1000)
+
 
 function scroll(){
     document.body.scrolltop = 0
     document.documentElement.scrolltop = 0
 }
 
+
+
+
+
+function formSubmit(){
+    
+}
 
